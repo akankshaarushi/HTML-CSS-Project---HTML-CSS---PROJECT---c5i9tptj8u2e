519d9ee0a1172578d951b9ff07b1a79245a94acb
akankshaarushi <115232645+akankshaarushi@users.noreply.github.com>
2022-12-09 10:08:38 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
 function home(){
               document.getElementById("button").style.background="url(img.jpg)";
-             document.getElementById("button").style.backgroundSize="cover"; 
-             document.getElementById("button").style.backgroundPosition="center"; 
 
+              setBackground();
 }
 function work(){
               document.getElementById("button").style.background="url(img1.jpg)";
-             document.getElementById("button").style.backgroundSize="cover";
-             document.getElementById("button").style.backgroundPosition="center"; 
+              setBackground();
 
 }
 function blog(){
               document.getElementById("button").style.background="url(img2.jpg)";
-             document.getElementById("button").style.backgroundSize="cover";
-             document.getElementById("button").style.backgroundPosition="center"; 
-
+              setBackground();
+             
 }
 function about(){
               document.getElementById("button").style.background="url(img3.jpg)";
-             document.getElementById("button").style.backgroundSize="cover";
-             document.getElementById("button").style.backgroundPosition="center"; 
+              setBackground();
 
 }
 
-
-
+function setBackground() {
+              document.getElementById("button").style.backgroundSize="cover";
+              document.getElementById("button").style.backgroundPosition="center"; 
+ 
+              
+}
